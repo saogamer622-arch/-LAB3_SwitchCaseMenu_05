cf7ff46f3c89376d762451d79485a60c5825fc87
saogamer622-arch <saogamer622@gmail.com>
2025-12-16 13:36:45 +0700

    LAB3_SwitchCaseMenu_05

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+class StudentService
+{
+    public void ShowMenu()
+    {
+        Console.WriteLine("=== Student Service Menu ===");
+        Console.WriteLine("1. ตรวจสอบเกรดเฉลี่ย");
+        Console.WriteLine("2. ตรวจสอบค่าเทอม");
+        Console.WriteLine("3. แสดงข้อมูลนักศึกษา");
+        Console.WriteLine("4. ออกจากโปรแกรม");
+        Console.Write("เลือกเมนู: ");
+    }
+
+    public void ProcessMenu(int menu)
+    {
+        switch (menu)
+        {
+            case 1:
+                ShowGPA();
+                break;
+            case 2:
+                ShowTuitionFee();
+                break;
+            case 3:
+                ShowStudentInfo();
+                break;
+            case 4:
+                Console.WriteLine("ออกจากโปรแกรม...");
+                break;
+            default:
+                Console.WriteLine("กรุณาเลือกเมนู 1-4 เท่านั้น");
+                break;
+        }
+
+        Console.WriteLine("----------------------------");
+    }
+
+    private void ShowGPA()
+    {
+        double gpa = 3.21;
+        Console.WriteLine("เกรดเฉลี่ย (GPA) ของคุณคือ: " + gpa);
+    }
+
+    private void ShowTuitionFee()
+    {
+        int fee = 15000;
+        Console.WriteLine("ค่าเทอมของคุณคือ: " + fee + " บาท");
+    }
+
+    private void ShowStudentInfo()
+    {
+        Console.WriteLine("ชื่อ: นายสมชาย ใจดี");
+        Console.WriteLine("รหัสนักศึกษา: 654123001");
+        Console.WriteLine("สาขา: เทคโนโลยีสารสนเทศ");
+    }
+}
